0c92d11ea413ddc1c2cff3ca153e8b069cc20fc1
ariesfun <ariesfun2019@outlook.com>
2024-03-14 10:53:39 +0800

    feat: add spdlog module

--- src/buried.cpp
+++ src/buried.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 
+#include <spdlog/spdlog.h>
+
 int BuriedTest()
 {
     std::cout << "Buried Test.";
+
+    // 设置日志级别为DEBUG
+    spdlog::set_level(spdlog::level::debug);
+
+    // 输出不同级别的日志
+    spdlog::debug("This is a debug message");
+    spdlog::info("This is an info message");
+    spdlog::warn("This is a warning message");
+    spdlog::error("This is an error message");
+
     return 1;
 }
